0d4150f2afc63876979e6c2ac4feb12652e21b6f
zhaolei <im.zhaolei@foxmail.com>
2021-03-18 11:16:42 +0800

    add rust impl leetcode question 28

---
+++ rust/question_28/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(unused)]
+/// https://leetcode-cn.com/problems/implement-strstr/
+
+struct Solution;
+
+impl Solution {
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        if needle.is_empty() {
+            return 0;
+        }
+        match haystack.find(&needle) {
+            Some(idx) => idx as i32,
+            None => -1,
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
