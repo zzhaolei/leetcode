91c24a7a13ebfee93eaec8b60a466876ddfccb80
zhaolei <im.zhaolei@foxmail.com>
2021-04-19 19:18:45 +0800

    improve question_27

--- rust/question_27/src/main.rs
+++ rust/question_27/src/main.rs
@@ -4,10 +4,7 @@
 struct Solution;
 
 impl Solution {
-    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-        if nums.is_empty() {
-            return 0;
-        }
+    fn impl_1(nums: &mut Vec<i32>, val: i32) -> i32 {
         let mut idx = 0;
         while idx < nums.len() {
             if nums[idx] == val {
@@ -18,10 +15,29 @@ impl Solution {
         }
         idx as i32
     }
+
+    fn impl_2(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let mut idx = 0;
+        let len = nums.len();
+        for i in 0..len {
+            if nums[i] != val {
+                nums[idx] = nums[i];
+                idx += 1;
+            }
+        }
+        idx as i32
+    }
+
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        // Solution::impl_1(nums, val)
+        Solution::impl_2(nums, val)
+    }
 }
 
 fn main() {
-    let mut nums = vec![0, 1, 2, 2, 3, 0, 4, 2];
-    let s = Solution::remove_element(&mut nums, 2);
-    println!("{}", s)
+    // let mut nums = vec![0, 1, 2, 2, 3, 0, 4, 2];
+    let s = Solution::remove_element(&mut vec![0, 1, 2, 2, 3, 0, 4, 2], 2);
+    assert_eq!(s, 5);
+    let s = Solution::remove_element(&mut vec![0, 1, 2, 2, 2, 3, 0, 4, 2], 2);
+    assert_eq!(s, 5);
 }
