c9f52dc56c2499f4f6db933c65f22e58208b98ae
zhaolei <im.zhaolei@foxmail.com>
2021-07-16 17:07:06 +0800

    add rust impl leetcode question 53

---
+++ rust/question_53/src/main.rs
@@ -0,0 +1,31 @@
+#![allow(unused)]
+/// https://leetcode-cn.com/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof/
+
+struct Solution;
+
+impl Solution {
+    fn impl_1(nums: Vec<i32>, target: i32) -> i32 {
+        nums.iter()
+            .filter(|e| if **e == target { true } else { false })
+            .collect::<Vec<&i32>>()
+            .len() as i32
+    }
+    fn impl_2(nums: Vec<i32>, target: i32) -> i32 {
+        let mut r = 0;
+        for v in nums.iter() {
+            if *v == target {
+                r += 1;
+            }
+        }
+        r
+    }
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        // Solution::impl_1(nums, target)
+        Solution::impl_2(nums, target)
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::search(vec![5, 7, 7, 8, 8, 10], 8), 2);
+    assert_eq!(Solution::search(vec![5, 7, 7, 8, 8, 10], 6), 0);
+}
