380a6718e52e2594915e49cff83b05f12e1491b4
zhaolei <im.zhaolei@foxmail.com>
2021-07-22 12:02:29 +0800

    add golang impl leetcode question 52

---
+++ golang/question_52/main.go
@@ -0,0 +1,49 @@
+// https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// getIntersectionNode 获取两个链表相交的节点
+// 相交的节点的指针地址相同
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	pa, pb := headA, headB
+	for pa != pb {
+		if pa == nil {
+			pa = headB
+		} else {
+			pa = pa.Next
+		}
+
+		if pb == nil {
+			pb = headA
+		} else {
+			pb = pb.Next
+		}
+	}
+	return pa
+}
+func main() {
+	a := &ListNode{
+		Val:  1,
+		Next: nil,
+	}
+	b := &ListNode{
+		Val:  2,
+		Next: nil,
+	}
+	getIntersectionNode(a, b)
+}
