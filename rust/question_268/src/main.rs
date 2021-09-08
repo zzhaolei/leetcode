f3248bbd69e552d6243b0248201e364cf50e87b1
zhaolei <im.zhaolei@foxmail.com>
2021-09-08 16:32:44 +0800

    add rust impl leetcode question 268

---
+++ rust/question_268/src/main.rs
@@ -0,0 +1,31 @@
+/// https://leetcode-cn.com/problems/missing-number/submissions/
+
+struct Solution;
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        let mut map: HashMap<&i32, Option<&i32>> = HashMap::new();
+        for i in nums.iter() {
+            map.insert(i, Some(i));
+        }
+        for i in 0..=nums.len() {
+            if let None = map.get(&(i as i32)) {
+                return i as i32;
+            }
+        }
+        0
+    }
+}
+
+fn main() {
+    println!("{}, {}", Solution::missing_number(vec![3, 0, 1]), 2);
+    println!("{}, {}", Solution::missing_number(vec![0, 1]), 2);
+    println!(
+        "{}, {}",
+        Solution::missing_number(vec![9, 6, 4, 2, 3, 5, 7, 0, 1]),
+        8
+    );
+    println!("{}, {}", Solution::missing_number(vec![0]), 1);
+}
