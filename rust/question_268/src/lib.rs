032661507553bda04849c961929757cf507116cc
zhaolei <im.zhaolei@foxmail.com>
2021-09-14 23:07:38 +0800

    refactor rust impl question 268

--- rust/question_268/src/lib.rs
+++ rust/question_268/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 /// https://leetcode-cn.com/problems/missing-number/submissions/
 
 struct Solution;
@@ -19,13 +21,15 @@ impl Solution {
     }
 }
 
-fn main() {
-    println!("{}, {}", Solution::missing_number(vec![3, 0, 1]), 2);
-    println!("{}, {}", Solution::missing_number(vec![0, 1]), 2);
-    println!(
-        "{}, {}",
-        Solution::missing_number(vec![9, 6, 4, 2, 3, 5, 7, 0, 1]),
-        8
-    );
-    println!("{}, {}", Solution::missing_number(vec![0]), 1);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn missing_number() {
+        assert_eq!(Solution::missing_number(vec![3, 0, 1]), 2);
+        assert_eq!(Solution::missing_number(vec![0, 1]), 2);
+        assert_eq!(Solution::missing_number(vec![9, 6, 4, 2, 3, 5, 7, 0, 1]), 8);
+        assert_eq!(Solution::missing_number(vec![0]), 1);
+    }
 }
