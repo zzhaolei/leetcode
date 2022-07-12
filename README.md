ba94899d3f31a162a89e8b70a053b5ccc76ef33f
dO.Ob - zhaolei <im.zhaolei@foxmail.com>
2022-07-12 17:07:41 +0800

    cells-with-odd-values-in-a-matrix

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # 解决方案
-> 全部题目：115
+> 全部题目：116
+> - 🟩 简单：85
 > - 🟥 困难：2
 > - 🟨 中等：29
-> - 🟩 简单：84
 
 |题目|难度|解答|
 |---|---|---|
@@ -100,6 +100,7 @@
 [1108. IP 地址无效化](https://leetcode.cn/problems/defanging-an-ip-address/)|🟩|[Rust](rust/src/bin/defanging-an-ip-address.rs)|
 [1189. “气球” 的最大数量](https://leetcode-cn.com/problems/maximum-number-of-balloons/)|🟩|[Rust](rust/src/bin/maximum-number-of-balloons.rs)|
 [1200. 最小绝对差](https://leetcode.cn/problems/minimum-absolute-difference/)|🟩|[Rust](rust/src/bin/minimum-absolute-difference.rs)|
+[1252. 奇数值单元格的数目](https://leetcode.cn/problems/cells-with-odd-values-in-a-matrix/)|🟩|[Rust](rust/src/bin/cells-with-odd-values-in-a-matrix.rs)|
 [1290. 二进制链表转整数](https://leetcode-cn.com/problems/convert-binary-number-in-a-linked-list-to-integer/)|🟩|[Rust](rust/src/bin/convert-binary-number-in-a-linked-list-to-integer.rs)|
 [1342. 将数字变成 0 的操作次数](https://leetcode-cn.com/problems/number-of-steps-to-reduce-a-number-to-zero/)|🟩|[Rust](rust/src/bin/number-of-steps-to-reduce-a-number-to-zero.rs)|
 [1380. 矩阵中的幸运数](https://leetcode-cn.com/problems/lucky-numbers-in-a-matrix/)|🟩|[Rust](rust/src/bin/lucky-numbers-in-a-matrix.rs)|
---
+++ rust/src/bin/cells-with-odd-values-in-a-matrix.rs
@@ -0,0 +1,64 @@
+struct Solution;
+
+impl Solution {
+    fn impl1(m: i32, n: i32, indices: Vec<Vec<i32>>) -> i32 {
+        let mut matrix = vec![vec![0; n as usize]; m as usize];
+        for i in indices.iter() {
+            let a = i[0] as usize;
+            let b = i[1] as usize;
+
+            for (j, v) in matrix.iter_mut().enumerate() {
+                v[b] += 1;
+                if j == a {
+                    for k in 0..v.len() {
+                        v[k] += 1;
+                    }
+                }
+            }
+        }
+        let mut ans = 0;
+        for v in matrix.iter() {
+            for k in v.iter() {
+                if k % 2 != 0 {
+                    ans += 1;
+                }
+            }
+        }
+        ans
+    }
+    fn impl2(m: i32, n: i32, indices: Vec<Vec<i32>>) -> i32 {
+        let mut rows = vec![0; m as usize];
+        let mut cols = vec![0; n as usize];
+        for i in indices {
+            rows[i[0] as usize] ^= 1;
+            cols[i[1] as usize] ^= 1;
+        }
+        let r: i32 = rows.iter().sum();
+        let c: i32 = cols.iter().sum();
+        r * (n - c) + c * (m - r)
+    }
+
+    pub fn odd_cells(m: i32, n: i32, indices: Vec<Vec<i32>>) -> i32 {
+        // Solution::impl1(m, n, indices)
+        Solution::impl2(m, n, indices)
+    }
+}
+
+fn main() {
+    println!("{}", Solution::impl1(2, 3, vec![vec![0, 1], vec![1, 1]]));
+    println!(
+        "{}",
+        Solution::odd_cells(2, 2, vec![vec![1, 1], vec![0, 0]])
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+
+    #[test]
+    fn test() {
+        assert_eq!(Solution::odd_cells(2, 3, vec![vec![0, 1], vec![1, 1]]), 6);
+        assert_eq!(Solution::odd_cells(2, 2, vec![vec![1, 1], vec![0, 0]]), 0);
+    }
+}
