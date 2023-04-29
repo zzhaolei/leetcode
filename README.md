a33f2a8b21566e9b32f3c26279623fd62c66ff3d
zhaolei <im.zhaolei@foxmail.com>
2023-04-29 12:21:53 +0800

    add remove-letter-to-equalize-frequency

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 解决方案
-> 全部题目：155
-> - 🟩 114
+> 全部题目：156
+> - 🟩 115
 > - 🟨 37
 > - 🟥 4
 
@@ -151,6 +151,7 @@
 [2315. 统计星号](https://leetcode.cn/problems/count-asterisks/)|🟩|[🦀](rust/src/bin/count-asterisks.rs)|
 [2409. 统计共同度过的日子数](https://leetcode.cn/problems/count-days-spent-together/)|🟩|[🦀](rust/src/bin/count-days-spent-together.rs) [🐹](golang/count-days-spent-together/main.go)|
 [2418. 按身高排序](https://leetcode.cn/problems/sort-the-people/)|🟩|[🦀](rust/src/bin/sort-the-people.rs)|
+[2423. 删除字符使频率相同](https://leetcode.cn/problems/remove-letter-to-equalize-frequency/)|🟩|[🦀](rust/src/bin/remove-letter-to-equalize-frequency.rs)|
 [2469. 温度转换](https://leetcode.cn/problems/convert-the-temperature/)|🟩|[🦀](rust/src/bin/convert-the-temperature.rs)|
 [剑指 Offer 06. 从尾到头打印链表](https://leetcode.cn/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/)|🟩|[🦀](rust/src/bin/cong-wei-dao-tou-da-yin-lian-biao-lcof.rs)|
 [剑指 Offer 22. 链表中倒数第k个节点](https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/)|🟩|[🦀](rust/src/bin/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof.rs)|
---
+++ rust/src/bin/remove-letter-to-equalize-frequency.rs
@@ -0,0 +1,73 @@
+struct Solution;
+
+impl Solution {
+    pub fn equal_frequency(word: String) -> bool {
+        use std::collections::{HashMap, HashSet};
+
+        let mut map: HashMap<char, u8> = HashMap::new();
+        for i in word.chars() {
+            match map.get_mut(&i) {
+                Some(value) => *value += 1,
+                None => {
+                    map.insert(i, 1);
+                }
+            }
+        }
+
+        let mut v = map.into_iter().map(|(_, v)| v).collect::<Vec<u8>>();
+        v.sort();
+        v.reverse();
+
+        let len = v.len();
+        if len == 1
+            || (len == 2 && v[len - 1] == 1)
+            || (v[0] == v[len - 1] && v[0] == 1)
+            || (v[len - 1] == 1
+                && v[..len - 1].iter().collect::<HashSet<&u8>>().iter().count() == 1)
+        {
+            return true;
+        }
+        (v[1..].iter().collect::<HashSet<&u8>>().iter().count() == 1) && v[0] - 1 == v[v.len() - 1]
+    }
+}
+
+// 1,1,2
+
+fn main() {
+    println!("1 false == {}", Solution::equal_frequency("ddaccb".into()));
+    println!("2 false == {}", Solution::equal_frequency("babbdd".into()));
+    println!("3 false == {}", Solution::equal_frequency("aazz".into()));
+    println!("4 true == {}", Solution::equal_frequency("abcc".into()));
+    println!("5 true == {}", Solution::equal_frequency("abc".into()));
+    println!("6 true == {}", Solution::equal_frequency("abbcc".into()));
+    println!("7 true == {}", Solution::equal_frequency("cccaa".into()));
+    println!("8 false == {}", Solution::equal_frequency("ccaa".into()));
+    println!("9 true == {}", Solution::equal_frequency("ca".into()));
+    println!("10 true == {}", Solution::equal_frequency("zz".into()));
+    println!("11 true == {}", Solution::equal_frequency("cccd".into()));
+    println!(
+        "12 false == {}",
+        Solution::equal_frequency("aaaabbbbccc".into())
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+
+    #[test]
+    fn test() {
+        assert!(!Solution::equal_frequency("ddaccb".into()));
+        assert!(!Solution::equal_frequency("babbdd".into()));
+        assert!(!Solution::equal_frequency("aazz".into()));
+        assert!(Solution::equal_frequency("abcc".into()));
+        assert!(Solution::equal_frequency("abc".into()));
+        assert!(Solution::equal_frequency("abbcc".into()));
+        assert!(Solution::equal_frequency("cccaa".into()));
+        assert!(!Solution::equal_frequency("ccaa".into()));
+        assert!(Solution::equal_frequency("ca".into()));
+        assert!(Solution::equal_frequency("zz".into()));
+        assert!(Solution::equal_frequency("cccd".into()));
+        assert!(!Solution::equal_frequency("aaaabbbbccc".into()));
+    }
+}
