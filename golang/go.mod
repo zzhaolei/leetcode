916480e7b6382a1e312aea09897884f217089cb4
zhaolei <im.zhaolei@foxmail.com>
2023-11-22 22:29:50 +0800

    update remove-nth-node-from-end-of-list

--- golang/go.mod
+++ golang/go.mod
@@ -1,3 +1,3 @@
 module golang
 
-go 1.16
+go 1.21
--- golang/remove-nth-node-from-end-of-list/main.go
+++ golang/remove-nth-node-from-end-of-list/main.go
@@ -35,8 +35,38 @@ func impl1(head *ListNode, n int) *ListNode {
 	return head
 }
 
+func impl2(head *ListNode, n int) *ListNode {
+	if head == nil {
+		return head
+	}
+	if n == 1 && head.Next == nil {
+		return nil
+	}
+	slowNode := head
+	nodeNext := head
+	s, f := 0, 0
+	for nodeNext != nil {
+		f++
+		// 当遍历完成时，slowNode的next节点正好就是要删除的节点
+		if f >= n+1 {
+			if s != 0 {
+				slowNode = slowNode.Next
+			}
+			s++
+		}
+		nodeNext = nodeNext.Next
+	}
+	if f == n {
+		return head.Next
+	}
+	// 删除节点
+	slowNode.Next = slowNode.Next.Next
+	return head
+}
+
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	return impl1(head, n)
+	//return impl1(head, n)
+	return impl2(head, n)
 }
 
 func main() {}
